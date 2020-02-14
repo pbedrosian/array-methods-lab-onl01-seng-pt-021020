e1bc0cac55e45beafca1c5371853cafe7261814d
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-02-14 02:47:28 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,34 @@
 def using_include(array, element)
+  new_array = array.include?(element)
 
+  return new_array
 end
 
 def using_sort(array)
+  new_array = array.sort
+
+  return new_array
 
 end
 
 def using_reverse(array)
+  new_array = array.reverse
 
 end
 
 def using_first(array)
+  new_array = array.first
+  new_array
 
 end
 
 def using_last(array)
+  new_array = array.last
+  new_array
 
 end
 
 def using_size(array)
+  new_array = array.size
 
 end
