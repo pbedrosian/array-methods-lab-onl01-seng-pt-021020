29a7a40a1e802884159bd8f3e84064b6a1abd6b2
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-03-06 18:16:47 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,10 @@
 def using_include(arr, n)
-  arr.include? n
+  arr.include? n #searches the array and looks to see if n is included in it
 end
 
 def using_sort(arr)
-  arr.sort
+  arr.sort #takes in the array and sorts it
 end
+
+def using_reverse(arr)
+  
