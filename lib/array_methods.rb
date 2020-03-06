9ab639a476bb415578bb6c0a736c677ef160e70f
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-03-06 18:24:53 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,34 +1,23 @@
-def using_include(array, element)
-  new_array = array.include?(element)
-
-  return new_array
+def using_include(arr, n)
+  arr.include? n #searches the array and looks to see if n is included in it
 end
 
-def using_sort(array)
-  new_array = array.sort
-
-  return new_array
-
+def using_sort(arr)
+  arr.sort #takes in the array and sorts it
 end
 
-def using_reverse(array)
-  new_array = array.reverse
-
+def using_reverse(arr)
+  arr.reverse #takes array and reverses the items
 end
 
-def using_first(array)
-  new_array = array.first
-  new_array
-
+def using_first(arr)
+  arr.first #takes in array and returns first item in array
 end
 
-def using_last(array)
-  new_array = array.last
-  new_array
-
+def using_last(arr)
+  arr.last #takes in array and returns last item
 end
 
-def using_size(array)
-  new_array = array.size
-
+def using_size(arr)
+  arr.length
 end
